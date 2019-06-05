139670410dfa93dfcef7fb6ea89acb5cda0a51d9
Avinash Kumar <44208272+avinashmallik62@users.noreply.github.com>
2019-06-05 13:51:32 +0530

    Add files via upload
    This is a simple interactive login app where a user can enter his details such as name ,email , password for signup and this reflects in the console.

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.avinash.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+
+        EditText myTextName =(EditText)findViewById(R.id.myTextName);
+        EditText myTextEmail =(EditText)findViewById(R.id.myTextEmail);
+        EditText myTextPassword =(EditText)findViewById(R.id.myTextPassword);
+
+
+        Log.i("Info",  myTextName.getText().toString());
+        Log.i("Info",  myTextEmail.getText().toString());
+        Log.i("Info",  myTextPassword.getText().toString());
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
